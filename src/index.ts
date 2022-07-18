bc4f63cd1ba39e4b2d0e4f7ea82c4d71a37698b0
Andrew Meyer <andrew.meyer@mongodb.com>
2022-07-18 20:45:16 +0200

    Basic sort

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ const applyAggregation = (aggregation: any, data: any) => {
         return applyMatchStage(query, data);
       case "$group":
         return applyGroupStage(query, data);
+			case "$sort":
+				return applySortStage(query, data)
       default: {
         throw new Error(`The stage parameter ${stage} has no implementation.`);
       }
@@ -60,5 +62,36 @@ const applyGroupStage = (query: any, data: any) => {
   }
   return results;
 };
+
+enum SortOrder{
+	Ascending= 1,
+	Descending= -1
+}
+
+
+const applySortStage = (query: any, data: any) => {
+		const field = Object.keys(query as any)[0];
+		const order: SortOrder = query[field]
+		const ascendingSort = (a: any, b: any) => {
+			if( a[field] < b[field]){
+				return -1
+			}
+			if( a[field] > b[field]){
+				return 1
+			}
+			return 0
+		}
+		const descendingSort = (a: any, b: any) => {
+			if( a[field] > b[field]){
+				return -1
+			}
+			if( a[field] < b[field]){
+				return 1
+			}
+			return 0
+		}
+		const sortFunction = order === SortOrder.Ascending ? ascendingSort: descendingSort;
+		return data.sort(sortFunction)
+}
 
 export { aggregate };
